89daa02b790a478800025475767a651a92d522ea
codes-by-pinewood <52709218+codes-by-pinewood@users.noreply.github.com>
2023-10-31 12:12:47 +0100

    Update StartupList.tsx

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,5 +1,39 @@
-import { Fragment, ReactElement } from "react";
+import { Fragment, ReactElement, useState, useEffect } from "react";
+import StartupMapper from "../../Http/Startup/Startup.mapper";
+import { Startup } from "../../Types/Startup";
+import Startupcomp from "./Startup";
 
 export default function StartupList(): ReactElement {
-  return <Fragment></Fragment>;
+  const [startups, setStartups] = useState<Startup[]>([]);
+
+  const getStartups = async () => {
+    const response = await fetch("/api/startups");
+    const startups = await response.json();
+    console.log(startups);
+    setStartups(startups);
+
+    return startups;
+  };
+
+  useEffect(() => {
+    getStartups();
+  }, []);
+
+  return (
+    <Fragment>
+      <ul>
+        {startups.length > 0 ? (
+          startups.map((startup) => {
+            return (
+              <li key={startup.id} className="list">
+                <Startupcomp startup={startup}></Startupcomp>
+              </li>
+            );
+          })
+        ) : (
+          <h1>no startups</h1>
+        )}
+      </ul>
+    </Fragment>
+  );
 }
